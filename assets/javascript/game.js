9d3e9d07d026305bd65acdc2410af3011506551e
Lcole.engr <lcole.engr@gmail.com>
2019-09-15 22:40:37 -0400

    Gameplay works, just need to define wins and losses and should be complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,117 @@
 
 
 
-
+var wins= 0;
+var losses=0;
 var targetNumber;
-$("target").text(targetNumber); // places or displays target number in "target" span 
 
-var userCounter = 0;
+
+var userCounter;
 var numberOptions = [];
 var crystalsrcs= ["./assets/images/diamond3.jpg", 
 "./assets/images/emerald2.jpg", "./assets/images/sapphire1.jpg","./assets/images/topaz1.jpg"]
 
+console.log(crystalsrcs);
 
-for (var h=0; h < 4; h++){
+
+// for (var i=0; i < 4; i++){
+    
+//     var crystalDiv = $("<div>");
+//     crystalDiv.addClass("col-3 my-col");
+
+//     var imageCrystal =$("<img>");
+//     imageCrystal.addClass("crystal-image");
+
+//     imageCrystal.attr("src", crystalsrcs[i]);
+
+//     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+//     $(".my-row3").append(imageCrystal);
+
+// console.log(imageCrystal);
+// console.log(i);
+// }
+
+
+
+
+
+
+
+function beginGame(){
+
+    userCounter=0;
+    
+
+    for (var h=0; h < 4; h++){
 
     var value = (Math.floor(Math.random() * 12) +1);
+    console.log(value);
     numberOptions.push(value);
+
+    console.log(numberOptions);
+}
+
+    targetNumber = (Math.floor(Math.random() *102) +19);
+    $("#target").html("<h1>" + targetNumber + "</h1>"); // places or displays target number in "target" span 
+
+    console.log(targetNumber);
+
+    
+
+
+
 }
 
 
-for (var i=0; i < numberOptions.length; i++){
+function winOrLose(){
+
+
+if (userCounter === targetNumber){
+    alert("You Win!");
+    wins++;
+    document.getElementById("wins").textContent = wins;
+
+    beginGame();
+
+}
+
+else if (userCounter > targetNumber){
+    alert("You Lose!");
+    losses++;
+    document.getElementById("losses").textContent = losses;
+
+    beginGame();
+}
+
+}
+
+
+
+
+$(document).ready(function(){
+
+
+// for (var i=0; i < 4; i++){
+    
+//     var crystalDiv = $("<div>");
+//     crystalDiv.addClass("col-3 my-col");
+
+//     var imageCrystal =$("<img>");
+//     imageCrystal.addClass("crystal-image");
+
+//     imageCrystal.attr("src", crystalsrcs[i]);
+
+//     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+//     $(".my-row3").append(imageCrystal);
+
+
+// }
+
+beginGame();
+
+for (var i=0; i < 4; i++){
     
     var crystalDiv = $("<div>");
     crystalDiv.addClass("col-3 my-col");
@@ -33,9 +126,7 @@ for (var i=0; i < numberOptions.length; i++){
     $(".my-row3").append(imageCrystal);
 
 
-
 }
-
 
 $(".crystal-image").on("click", function(){
 
@@ -43,18 +134,24 @@ $(".crystal-image").on("click", function(){
 var crystalValue = ($(this).attr("data-crystalvalue"));
 crystalValue = parseInt(crystalValue);
 
-counter += crystalValue;
-
-if (counter === targetNumber){
-    alert("You Win!");
-}
-
-else if (counter > targetNumber){
-    alert("You Lose!");
-}
+userCounter += crystalValue;
 
 
-
+console.log(crystalValue);
+$("#currentValue").html("<h1>" + userCounter + "</h1>");
 
 });
 
+
+
+winOrLose();
+
+
+
+
+
+
+
+
+
+})
